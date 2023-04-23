6bc16badd2d05467c3341d032db121611dcaa1c1
Lalindu M Rubasinghe <mihisara.r99@hotmail.com>
2023-04-23 20:26:46 +0530

    frontend edited

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
-import LoginPage from './Components/LoginPage/LoginPage';
-//import Home from './Components/HomePage/Home';
 import './App.css';
+import LoginPage from "./Components/LoginPage/LoginPage";
+import HomePage from "./Components/HomePage/HomePage";
 
 function App() {
-  return (
-    <div className="App">
-    
-          <LoginPage/> 
-    </div>
-  );
+    return (
+        <div className="App">
+           <LoginPage/>: <HomePage/>
+           
+        </div>
+    );
 }
 
 export default App;
--- frontend/src/Components/MainContent/MainContent.js
+++ frontend/src/Components/MainContent/MainContent.js
@@ -1,38 +1,29 @@
-import React, { Component } from 'react';
-import "./MainContent.css";
-import { Grid } from '@mui/material';
-import StatusBar from '../StatusBar/StatusBar';
-import MainPage from '../MainPage/MainPage';
-import InfoSection from '../InfoSection/InfoSection';
-import Suggestions from '../Suggestions/Suggestions';
+import './MainContent.css'
+import Grid from "@mui/material/Grid";
+import MainPage from "../MainPage/MainPage";
+import StatusBar from "../StatusBar/StatusBar";
+import InfoSection from "../InfoSection/InfoSection";
+import Suggestion from "../Suggestion/Suggestion";
 
-
-class MainContent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return ( 
-            <div>
-                <Grid container>
-                    <Grid item xs={2}></Grid>
-                    <Grid item xs={6} className="maincontent__container">
-                        <div>
-                            <StatusBar />
-                            <MainPage />
-                        </div>
-                    </Grid>
-                    <Grid item xs={2}>
-                        <InfoSection />
-                        <Suggestions />
-                    </Grid>
-                    <Grid item xs={2}>
-                    </Grid>
+function MainContent() {
+    return (
+        <div>
+            <Grid container>
+                <Grid item xs={2}></Grid>
+                <Grid item xs={6}>
+                    <div>
+                        <StatusBar/>
+                        <MainPage/>
+                    </div>
                 </Grid>
-            </div>
-         );
-    }
+                <Grid item xs={2}>
+                    <InfoSection/>
+                    <Suggestion/>
+                </Grid>
+                <Grid item xs={2}></Grid>
+            </Grid>
+        </div>
+    )
 }
- 
+
 export default MainContent;
