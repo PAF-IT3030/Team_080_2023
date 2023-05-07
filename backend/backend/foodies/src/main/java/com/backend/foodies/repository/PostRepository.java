c10c9841d0b848bd2495898da17091a7543e2220
Sapooth Sanjaya <90241804+Sapooth@users.noreply.github.com>
2023-05-07 18:14:41 +0530

    Backend Done

--- backend/backend/foodies/src/main/java/com/backend/foodies/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.backend.foodies.repository;
-
-import com.backend.foodies.model.Post;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PostRepository extends MongoRepository <Post, Integer> {
-}
--- backend/backend/foodies/src/main/java/com/backend/foodies/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.backend.foodies.repository;
-
-import com.backend.foodies.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User,Integer> {
-
-}
